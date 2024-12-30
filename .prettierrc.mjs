7b5b14860e4b202b52d4e4dde3b6a0f5661012e8
gallianoms <ggallianoms@gmail.com>
2024-12-30 14:40:53 +0100

    chore: add config astro, tailwindcss, shadcn

---
+++ .prettierrc.mjs
@@ -0,0 +1,27 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ],
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  quoteProps: 'as-needed',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: true
+};
